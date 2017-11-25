ccf5a62affbd1313e47e488803786daccaa83016
wisemanchild <wisemanchild@outlook.com>
2017-11-25 13:09:51 -0800

    move the project menu options to this file

--- templates/nav.php
+++ templates/nav.php
@@ -1,20 +1,13 @@
  <nav column="8" class="nav">
-
-      <ul>
-
-        <li><a href="books.php">Books</a></li>
-
-        <li><a href="#">Stories</a></li>
-
-        <li><a href="#">Quotes</a></li>
-
-        <li><a href="login.php">Login</a></li>
-
-        <li><a href="register.php">Register</a></li>
-		  
-        <li><a href="posting.php">Post</a></li>
-
-
-     </ul>
+				<ul>
+					<li><a href="#hero">HOME</a></li>
+					<li><a href="#about">ABOUT</a></li>
+					<li> <a href="#contact">CONTACT</a></li>
+					<li> <a href="pages/life.html">LIFE</a></li>
+					<li> <a href="pages/health.html">HEALTH</a></li>
+					<li> <a href="pages/strength.html">STRENGTH</a></li>
+					<li> <a href="pages/wealth.html">WEALTH</a></li>
+					<li> <a href="pages/wisdom.html">WISDOM</a></li>
+				</ul>
 
     </nav>
