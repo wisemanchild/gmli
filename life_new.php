071573be3d66656c2e41d33eb33e2ca202ddf96d
wisemanchild <wisemanchild@outlook.com>
2017-12-12 05:33:18 -0800

    version 1.5 with updated heading all with includes

---
+++ life_new.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html><!-- InstanceBegin template="/Templates/gmli_template.dwt.php" codeOutsideHTMLIsLocked="false" -->
+<meta charset="utf-8">
+<!-- InstanceBeginEditable name="info" -->	
+<?php
+
+/*-- PHP FILE INFO -------------------------------------------------------------
+ *  Application: WEBD168 Final css project           
+ *  Description: life page for the gmli website
+ *    File Name: life_new.php
+ *       Author: Norman McWilliams     Tester:
+ * Date created: 12-10-2017            Date updated: 12-12-2017
+ * Time created: 8:35pm          Time updated: 5:32am
+ *    Revisions: 1.5
+ *    Copyright: (c) 2017 Norlab Business Solutions
+ *----------------------------------------------------------------------------- */
+?>
+
+<!-- InstanceEndEditable -->
+<!-- InstanceBeginEditable name="head" --> 
+<head>
+	<meta name="description" content="main section of the Norlab Business Solution website project">
+	<meta name="keywords" content="Life, Health, Strength, Wealth, Wisdom, Power, Love, Spritual">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Untitled Document</title>
+
+	<link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+	<link href="css/singlePageTemplate.css" rel="stylesheet" type="text/css">	
+	<link href="css/nshift360gmli_lib.css" rel="stylesheet" type="text/css">
+	<style>
+		<!--styles go here -->
+	 <br>
+	
+	</style>
+
+</head>
+<!-- InstanceEndEditable -->
+
+
+
+<body>
+<!-- InstanceBeginEditable name="body" -->
+
+	<?php include_once('includes/header.php') ?>
+	<?php include('includes/functions.php') ?>
+
+	
+			<section class="banner" title="Life Courses">
+			<h2 class="parallax">LIFE COURSES</h2>
+			<p class="parallax_description"> life resources and solutions and discussions regarding the lives of human beings on this Earth.. <br> more...
+
+			</p>
+
+			<p class="parallax_description">&nbsp;</p>
+
+		</section>
+
+
+	<?php include_once('includes/catagories.php'); ?>
+	<!-- Footer Section -->
+	<?php include_once('includes/footer.php'); ?>
+	<!-- footer menu -->
+	<!-- Main Container Ends -->
+	
+<!-- InstanceEndEditable -->	
+</body>
+
+
+<!-- InstanceEnd --></html>
