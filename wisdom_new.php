aa98fa883f548e9419c035fe63bf5314507b89ce
wisemanchild <wisemanchild@outlook.com>
2017-12-12 05:27:56 -0800

    version 1.5 with all include files

---
+++ wisdom_new.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html><!-- InstanceBegin template="/Templates/gmli_template.dwt.php" codeOutsideHTMLIsLocked="false" -->
+<meta charset="utf-8">
+<!-- InstanceBeginEditable name="info" -->	
+<?php
+
+/*-- PHP FILE INFO -------------------------------------------------------------
+ *  Application: WEBD166 Final Quote database project
+ *  Description: application function is to display random quotes ...
+ *    File Name: xxx.php
+ *       Author: Norman McWilliams     Tester:
+ * Date created: 12-03-2017            Date updated: 12-03-2017
+ * Time created: 8:35pm          Time updated: 8:35pm
+ *    Revisions: 1.0
+ *    Copyright: (c) 2017 Norlab Business Solutions
+ *----------------------------------------------------------------------------- */
+?>
+
+<!-- InstanceEndEditable -->
+<!-- InstanceBeginEditable name="head" --> 
+<head>
+	<meta name="description" content="main section of the Norlab Business Solution website project">
+	<meta name="keywords" content="Life, Health, Strength, Wealth, Wisdom, Power, Love, Spritual">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Untitled Document</title>
+
+	<link href="solutions/css/bootstrap.css" rel="stylesheet" type="text/css">
+	<link href="css/singlePageTemplate.css" rel="stylesheet" type="text/css">	
+	<link href="solutions/css/nshift360gmli_lib.css" rel="stylesheet" type="text/css">
+	<style>
+		<!--styles go here -->
+	</style>
+
+</head>
+<!-- InstanceEndEditable -->
+
+
+
+<body>
+<!-- InstanceBeginEditable name="body" -->
+	<header class="center-block" title="Wisdom Courses">
+	<?php include_once('includes/header.php') ?>
+	</header>
+<section class="banner" title="Wisdom Courses">
+  <h2 class="parallax">WISDOM COURSES</h2>
+  <p class="parallax_description"> Resources, solutions and discussions regarding the concepts of knowledge and wisdom..
+   <br>
+   more...</p>
+    <p class="parallax_description">&nbsp;</p>
+  </section>
+	
+
+	<?php include_once('includes/catagories.php'); ?>
+	<!-- Footer Section -->
+	<?php include_once('includes/footer.php'); ?>
+	<!-- footer menu -->
+	<!-- Main Container Ends -->
+	
+<!-- InstanceEndEditable -->	
+</body>
+
+
+<!-- InstanceEnd --></html>
