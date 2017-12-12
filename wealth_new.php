55bee983ed9fad289ff92a675b4ae9d47516482b
wisemanchild <wisemanchild@outlook.com>
2017-12-12 05:31:11 -0800

    version 1.5 with all includes and updated heading

---
+++ wealth_new.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html><!-- InstanceBegin template="/Templates/gmli_template.dwt.php" codeOutsideHTMLIsLocked="false" -->
+<meta charset="utf-8">
+<!-- InstanceBeginEditable name="info" -->	
+<?php
+
+/*-- PHP FILE INFO -------------------------------------------------------------
+ *  Application: WEBD168 Final css projet
+ *  Description: wealth page for the gmli website
+ *    File Name: wealth_new.php
+ *       Author: Norman McWilliams     Tester:
+ * Date created: 12-03-2017            Date updated: 12-12-2017
+ * Time created: 8:35pm          Time updated: 5:29am
+ *    Revisions: 1.5
+ *    Copyright: (c) 2017 Norlab Business Solutions
+ *----------------------------------------------------------------------------- */
+?>
+
+<!-- InstanceEndEditable -->
+<!-- InstanceBeginEditable name="head" --> 
+<head>
+	<meta name="description" content="main section of the Norlab Business Solution website project">
+	<meta name="keywords" content="Life, Health, Strength, Wealth, Wisdom, Power, Love, Spritual">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Untitled Document</title>
+
+	<link href="solutions/css/bootstrap.css" rel="stylesheet" type="text/css">
+	<link href="css/singlePageTemplate.css" rel="stylesheet" type="text/css">	
+	<link href="solutions/css/nshift360gmli_lib.css" rel="stylesheet" type="text/css">
+	<style>
+		<!--styles go here -->
+	</style>
+
+</head>
+<!-- InstanceEndEditable -->
+
+
+
+<body>
+<!-- InstanceBeginEditable name="body" -->
+	<header class="center-block" title="Wealth Courses">
+	<?php include_once('includes/header.php') ?>
+	</header>
+	
+	<!-- Hero Section -->
+	
+	
+	<!-- About Section -->
+
+	<section class="about" id="about">
+		<h2 class="hidden">About</h2>
+	</section>
+
+	<!-- Parallax Section -->
+  <section class="banner" title="Wealth Courses">
+    <h2 class="parallax">WEALTH MANAGEMENT</h2>
+    <p class="parallax_description">Wealth is not just the acquisition of money there's also the possibility of not thinking about money and working towards worthy goals...<br>
+    </p>
+    <p class="parallax_description">&nbsp;</p>
+  </section>
+
+	
+	<?php include_once('includes/catagories.php'); ?>
+	<!-- Footer Section -->
+	<?php include_once('includes/footer.php'); ?>
+	<!-- footer menu -->
+	<!-- Main Container Ends -->
+	
+<!-- InstanceEndEditable -->	
+</body>
+
+
+<!-- InstanceEnd --></html>
