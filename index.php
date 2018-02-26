275be7a8f245fb0b4d935d5dc7ad5a9b407619aa
wisemanchild <wisemanchild@outlook.com>
2018-02-25 21:13:50 -0800

    starting to remove unnecessary css and use scss preprocessor coding techniques

--- index.php
+++ index.php
@@ -28,8 +28,8 @@
 
 	<link href="css/bootstrap.css" rel="stylesheet" type="text/css">
 	<link href="css/singlePageTemplate.css" rel="stylesheet" type="text/css">	
-	<link href="css/nshift360gmli_lib.css" rel="stylesheet" type="text/css">
-
+<!--	<link href="css/nshift360gmli_lib.css" rel="stylesheet" type="text/css">-->
+	<link href="css/gmli_lib.css" type="text/css" rel="stylesheet">
 	<style>
 		<!--
 		/* nlm-11-25-2017 begin style section */
@@ -46,7 +46,7 @@
 		/* nlm-11-25-2017  position is relative and width is 100% because the video is contained in a container class that has a 90% width */
 
 		#myVideo {
-			position: sticky;
+			position:relative;
 			right: 0;
 			bottom: 0;
 			top: 100;
@@ -117,17 +117,17 @@
     <h1 class="hero_header"><a href="#about"> <span class="glyphicon glyphicon-triangle-bottom"></span></a></h1>
 		<!-- nlm-11-25-2017 need to find a better way of rapping content inside the video -->
 <div class="row">
-<video autoplay muted loop id="myVideo" class="col-lg-4" >
+<!--<video autoplay muted loop id="myVideo">
   <source src="video/logo_video.mp4" type="video/mp4" >
   Your browser does not support HTML5 video.
 </video>
-	</div>		
-
-<div class="section">
+-->	</div>		
+	
+<!--<div class="section">
   <h1>Heading</h1>
   <p>Lorem ipsum dolor sit amet, an his etiam torquatos. Tollit soleat phaedrum te duo, eum cu recteque expetendis neglegentur. Cu mentitum maiestatis persequeris pro, pri ponderum tractatos ei. Id qui nemore latine molestiae, ad mutat oblique delicatissimi pro.</p>
   <button id="myBtn" onclick="myFunction()">Pause</button>
-</div>
+</div>-->
 
 <script>
 var video = document.getElementById("myVideo");
@@ -164,11 +164,13 @@ function myFunction() {
 	<main class="main-article">
 		<h2>Welcome to the Institute Page</h2>
 		<p>Welcome to the Grace and Mercy Institute Website. We are a non-profit organization specizing in working with people who are interested in making changes to this world that will make a difference for what people think about and what they care about. The philosopy of the Institute is to deal with the six main topic that are daily challenges for all of us.
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, quae repellendus! Dolore quas magni omnis incidunt voluptatum natus blanditiis dolorem eligendi, consequuntur? Voluptatum in esse accusamus aspernatur amet saepe vel, aliquid aliquam enim. Eveniet adipisci, officiis ullam fugiat tempora sunt, voluptatibus atque accusantium ad, cum commodi, doloremque quasi laboriosam quis!</p>
-		<p><quote>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore provident odit ea facilis nihil! Reprehenderit ipsum possimus odio corporis labore veniam sed sint rem eius porro, minima voluptatibus quasi exercitationem alias delectus dolorum at error architecto soluta quas esse, eum. Quos deserunt quod molestiae ea distinctio cum, tenetur dicta harum debitis itaque suscipit quidem facilis ducimus, saepe vel facere quo quibusdam officiis repudiandae possimus, earum excepturi iusto, nemo ut dolore? Iure eaque, rem optio aspernatur!</quote></p>
+		<p>These six main topics are: Life, Health, Strength, Wealth, and Wisdom. &nbsp;We deal with these topics everyday. &nbsp;Not necessarily in this order but do deal with them</p>
+		<p>        
+		<p>            
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore provident odit ea facilis nihil! Reprehenderit ipsum possimus odio corporis labore veniam sed sint rem eius porro, minima voluptatibus quasi exercitationem alias delectus dolorum at error architecto soluta quas esse, eum. Quos deserunt quod molestiae ea distinctio cum, tenetur dicta harum debitis itaque suscipit quidem facilis ducimus, saepe vel facere quo quibusdam officiis repudiandae possimus, earum excepturi iusto, nemo ut dolore? Iure eaque, rem optio aspernatur!</p>
 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque, omnis porro cumque animi nihil nulla obcaecati, at, corporis tempora nobis dolorem minima! Dignissimos, molestiae, pariatur.</p>
 		</p>
-		<h2 title="life"><a href="life_new.php" class= "glyphicon glyphicon-link">Life Courses</a></h2>
+		<h2 title="life"><a href="life_new.php" class= "glyphicon glyphicon-link glyphicon small">Life Courses</a></h2>
 		<p>We provide resources and solutions and discussions regarding the lives of human beings on this earth</p>
 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi maiores molestias beatae voluptatibus perferendis voluptas totam id quasi ad qui!</p>
 		
