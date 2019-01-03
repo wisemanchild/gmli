bc2bd67d92a30dc97fafe5a7425115cd41c4f8f0
wisemanchild <wisemanchild@outlook.com>
2019-01-03 09:31:20 -0800

    resized logo icon

--- includes/header.php
+++ includes/header.php
@@ -4,15 +4,14 @@ ob_start();
 ?>
 <style>
 	header h1 {
-		font-size: 4em;
+		font-size: 3em;
 		color: white;
 		text-shadow: 4px 4px #ff0000;
 		width: 100%;
 		height: 15%;
-
 	}
 </style>
-<?php include_once('nav.php')  ?>
-<img src="images/logo.png" class="logorotate col-sm-4" width="100" height="100" alt="Norlab Business Solutions company logo"/>	
-<h1 class="col-sm-4">Norlab Business Solutions</h1>
+<?php include('nav.php')  ?>
+<img src="../images/logo.png" class="logorotate col-sm-2" width="150" height="150" alt="Norlab Business Solutions company logo"/>
+<h1 class="col-sm-8">Norlab Business Solutions</h1>
 
