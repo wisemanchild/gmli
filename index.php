16f7f79bf3c1b1e9fb5b2d667b23c8a231555b1b
wisemanchild <wisemanchild@outlook.com>
2019-01-05 10:57:36 -0800

    Several bugs fixed in this revision: 1. move back to bootstrap 3.0 for the free glyphicons and no real need for bootstrap 4.0 2. Corrected red shadow in brand section of navbar 3. Cleaned up gap between hero and banner sections due to incorrect included css links 4. cleaned up pseudo-elements 5. changed to a better border pictures 6. Added 3d logo on click

--- index.php
+++ index.php
@@ -1,10 +1,7 @@
 <!doctype html>
 
 <html>
-<!-- InstanceBegin template="/Templates/gmli_template.dwt.php" codeOutsideHTMLIsLocked="false" -->
-
 <meta charset="utf-8">
-<!-- InstanceBeginEditable name="info" -->
 <?php
 
 /*-- PHP FILE INFO -------------------------------------------------------------
@@ -12,9 +9,9 @@
  *  Description: main page on the gmli website
  *    File Name: health_new.php
  *       Author: Norman McWilliams     Tester:
- * Date created: 12-03-2017            Date updated: 01-03-2019
+ * Date created: 12-03-2017            Date updated: 01-05-2019
  * Time created: 8:35pm          Time updated: 5:12am
- *    Revisions: 1.6
+ *    Revision: 1.7
  *    Copyright: (c) 2017 Norlab Business Solutions
  *----------------------------------------------------------------------------- */
 ?>
@@ -24,132 +21,26 @@
     <meta name="description" content="main section of the Norlab Business Solution website project">
     <meta name="keywords" content="Life, Health, Strength, Wealth, Wisdom, Power, Love, Spritual">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Grace and Mercy Institute Home Page</title>
-
-    <link href="css/bootstrap-4.1.3.css" rel="stylesheet" type="text/css">
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
     <link href="css/singlePageTemplate.css" rel="stylesheet" type="text/css">
     <link href="css/nshift360gmli_lib.css" rel="stylesheet" type="text/css">
-    <link rel="icon" type="image/png" href="images/logo_nshift360_16x16.png"/>
+    <link rel="icon" type="image/png" href="images/logo.png"/>
 
-
-    <style>
-
-        <!--
-
-        /* nlm-11-25-2017 begin style section */
-
-        .banner {
-
-            background-color: #2D9AB7;
-            background-image: url(images/parallax_teacher-helping-mature-students-in-library.png);
-            height: 400px;
-            background-attachment: absolute;
-            background-size: cover;
-            background-repeat: no-repeat;
-        }
-
-        /* nlm-11-25-2017  position is relative and width is 100% because the video is contained in a container class that has a 90% width */
-        #myVideo {
-            position: sticky;
-            right: 0;
-            bottom: 0;
-            top: 100;
-
-            width: 100%;
-
-            height: 100%;
-
-        }
-
-        .content {
-
-            position: relative;
-
-            bottom: 0;
-
-            background: rgba(0, 0, 0, 1.0);
-
-            color: #f1f1f1;
-
-            width: 90%;
-
-            padding: 0px;
-
-        }
-
-        @import url('https://fonts.googleapis.com/css?family=Anton|Playfair+Display+SC:700i');
-
-        h1, h2, h3, h4, h5, h6 {
-            font-family: 'Playfair Display SC', serif;
-        }
-
-        p {
-            font-family: 'Anton', sans-serif;
-
-        }
-
-        #header nav {
-
-            background-color: green;
-
-        }
-
-        main h2 {
-
-            font-size: 3em;
-
-        }
-
-        .hero_header a {
-
-            color: white;
-
-            font-size: 2em;
-
-        }
-
-        quote {
-
-            font-family: cursive;
-
-            color: green;
-
-            margin: 150px;
-
-            padding: 50px;
-
-            text-align: center;
-
-        }
-
-        p::first-letter {
-
-            font-size: 1.5em;
-
-        }
-
-        #hero {
-            background-position: center;
-            width: 100%;
-        }
-    </style>
-
+    <!-- Section to repair broker glyphicons-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <!--    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	-->
 
 </head>
 
-
 <body>
 
-
-<!-- Main Container -->
-
-<!-- Navigation -->
-
-<header class="center-block">
-
-	<?php include_once( 'includes/header.php' ) ?>
-
+<header class="center-block" title="Grace and Mercy Institute Home Page">
+	<?php include_once 'includes/header.php'; ?>
 </header>
 
 <!-- nlm-11-25-2017 logo dimensions are too large for current size of navbar -->
@@ -157,54 +48,45 @@
 
 <!-- Hero Section -->
 
-
 <section class="hero container-fluid" id="hero">
-    <h1 class="hero_header">Norlab Business <span class="light">Solutions</span></h1>
-    <p class="tagline">Send it to the Lab!</p>
+    <!--   <h1 class="hero_header">Norlab Business <span class="light">Solutions</span></h1>
+	   <p class="tagline">Send it to the Lab!</p>
 
-    <video id="myVideo" onended="videoEnded()" width="100%" height="100%" autoplay>
-        <source src="video/logo_video.mp4" type="video/mp4">
-        your browser doesn't support
-    </video>
-    <script>
+	   <video id="myVideo" onended="videoEnded" width="100%" height="100%" autoplay>
+		   <source src="video/logo_video.mp4" type="video/mp4">
+		   your browser doesn't support
+	   </video>
 
-    </script>
+	   <script>
 
-    <h1 class="hero_header"><a href="#about"> <span class="glyphicon glyphicon-triangle-bottom"></span></a></h1>
+	   </script>
 
-    <!-- nlm-11-25-2017 need to find a better way of rapping content inside the video -->
+	   <h1 class="hero_header"><a href="#about"> <span class="glyphicon glyphicon-triangle-bottom"></span></a></h1>
 
-</section>
+   --></section>
 
 
 <!-- About Section -->
 
-
+<!--
 <section class="about" id="about">
-
-    <h2 class="hidden">About</h2>
-
 </section>
-
+-->
 
 <!-- Parallax Section -->
 
 
 <section class="banner" title="Grace And Mercy Learning Institute">
-
     <h2 class="parallax">GRACE AND MERCY LEARNING INSTITUTE</h2>
-
     <p class="parallax_description">The Grace and Mercy Training Institute is a subsidiary of Norlab Business Solutions
         that has a three tier train approach for three generations of people. <br> more...
-
     </p>
 
     <p class="parallax_description">&nbsp;</p>
-
 </section>
 
 
-<main class="main-article">
+<main class="main-article" id="about">
 
     <h2>Welcome to the Institute Page</h2>
 
@@ -212,25 +94,22 @@
         people who are interested in making changes to this world that will make a difference for what people think
         about and what they care about. The philosopy of the Institute is to deal with the six main topic that are daily
         challenges for all of us.
-
+    </p>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, quae repellendus! Dolore quas magni omnis
         incidunt voluptatum natus blanditiis dolorem eligendi, consequuntur? Voluptatum in esse accusamus aspernatur
         amet saepe vel, aliquid aliquam enim. Eveniet adipisci, officiis ullam fugiat tempora sunt, voluptatibus atque
-        accusantium ad, cum commodi, doloremque quasi laboriosam quis!</p>
-
-    <p>
-        <quote>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore provident odit ea facilis nihil!
-            Reprehenderit ipsum possimus odio corporis labore veniam sed sint rem eius porro, minima voluptatibus quasi
-            exercitationem alias delectus dolorum at error architecto soluta quas esse, eum. Quos deserunt quod
-            molestiae ea distinctio cum, tenetur dicta harum debitis itaque suscipit quidem facilis ducimus, saepe vel
-            facere quo quibusdam officiis repudiandae possimus, earum excepturi iusto, nemo ut dolore? Iure eaque, rem
-            optio aspernatur!
-        </quote>
+        accusantium ad, cum commodi, doloremque quasi laboriosam quis!
     </p>
-
+    <blockquote>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore provident odit ea facilis nihil!
+        Reprehenderit ipsum possimus odio corporis labore veniam sed sint rem eius porro, minima voluptatibus quasi
+        exercitationem alias delectus dolorum at error architecto soluta quas esse, eum. Quos deserunt quod
+        molestiae ea distinctio cum, tenetur dicta harum debitis itaque suscipit quidem facilis ducimus, saepe vel
+        facere quo quibusdam officiis repudiandae possimus, earum excepturi iusto, nemo ut dolore? Iure eaque, rem
+        optio aspernatur!
+    </blockquote>
+    
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque, omnis porro cumque animi nihil nulla
-        obcaecati, at, corporis tempora nobis dolorem minima! Dignissimos, molestiae, pariatur.</p>
-
+         obcaecati, at, corporis tempora nobis dolorem minima! Dignissimos, molestiae, pariatur.
     </p>
 
     <h2 title="life"><a href="life_new.php" class="glyphicon glyphicon-link">Life Courses</a></h2>
@@ -238,7 +117,8 @@
     <p>We provide resources and solutions and discussions regarding the lives of human beings on this earth</p>
 
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi maiores molestias beatae voluptatibus
-        perferendis voluptas totam id quasi ad qui!</p>
+        perferendis voluptas totam id quasi ad qui!
+    </p>
 
 
     <h2 title="health"><a href="health_new.php" class="glyphicon glyphicon-link">Health Courses</a></h2>
@@ -275,11 +155,11 @@
 <!-- More Info Section -->
 
 
-<?php include_once( 'includes/catagories.php' ); ?>
+<?php include_once 'includes/catagories.php'; ?>
 
 <!-- Footer Section -->
 
-<?php include_once( 'includes/footer.php' ); ?>
+<?php include_once 'includes/footer.php'; ?>
 
 <!-- footer menu -->
 
@@ -287,13 +167,15 @@
 
 <script src="js/popper.min.js"></script>
 <script src="js/bootstrap-4.1.3.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>-->
 
 <script>
     /*
     *   remove the spashscreen video and hero title display
     *   TODO: need to find a way to reduce the wait time on vid.style.display ended event
     */
+/*
+
     function videoEnded() {
         var vid = document.getElementById("myVideo");
         var hero = document.getElementById("hero");
@@ -302,23 +184,21 @@
         hero.style.display = "none";
     }
 
-    /*
-		function endOfSpashVideo() {
-		var vid = document.getElementById("myVideo");
-			if var.played.end >
-	alert("Start: " + vid.played.start(0) + " End: " + vid.played.end(0));
-	alert("Video is ended")
-			alert(<br> + var.played.end)
 
+    function endOfSpashVideo() {
+        var vid = document.getElementById("myVideo");
+        if vid.played.end >
+            alert("Start: " + vid.played.start(0) + " End: " + vid.played.end(0));
+        alert("Video is ended")
+        alert( < br > +
+        var .
+        played.end
+    )
 
-	*/
-
+*/
 
 </script>
 
 </body>
-
-</body>
-
 
 </html>
